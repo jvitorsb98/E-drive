935de1cbde46cae8a1fa14f0a7f069ed6e7c0fed
João Ramos <jvitorsb98@outlook.com>
2024-07-29 17:22:16 -0300

    Backend started

---
+++ Back-end/src/main/java/br/com/cepedi/e_drive/EDriveApplication.java
@@ -0,0 +1,13 @@
+package br.com.cepedi.e_drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDriveApplication.class, args);
+	}
+
+}
