3f14ced40af45f2a210938371c33e32b5cc95949
João Ramos <jvitorsb98@outlook.com>
2024-07-30 11:21:16 -0300

    Seeders concluidos

---
+++ Back-end/src/main/resources/db/data/S4__insert_category.sql
@@ -0,0 +1,15 @@
+-- Inserindo categorias na tabela "category"
+INSERT INTO "category" ("id", "name", "activated") VALUES (1, 'Sub Compacto', true);
+INSERT INTO "category" ("id", "name", "activated") VALUES (2, 'Compacto', true);
+INSERT INTO "category" ("id", "name", "activated") VALUES (3, 'Médio', true);
+INSERT INTO "category" ("id", "name", "activated") VALUES (4, 'Grande', true);
+INSERT INTO "category" ("id", "name", "activated") VALUES (5, 'Extra Grande', true);
+INSERT INTO "category" ("id", "name", "activated") VALUES (6, 'Utilitário Esportivo Compacto', true);
+INSERT INTO "category" ("id", "name", "activated") VALUES (7, 'Utilitário Esportivo Grande 4x4', true);
+INSERT INTO "category" ("id", "name", "activated") VALUES (8, 'Fora de Estrada Compacto', true);
+INSERT INTO "category" ("id", "name", "activated") VALUES (9, 'Fora de Estrada Grande', true);
+INSERT INTO "category" ("id", "name", "activated") VALUES (10, 'Minivan', true);
+INSERT INTO "category" ("id", "name", "activated") VALUES (11, 'Comercial', true);
+INSERT INTO "category" ("id", "name", "activated") VALUES (12, 'Picape Compacta', true);
+INSERT INTO "category" ("id", "name", "activated") VALUES (13, 'Picape', true);
+INSERT INTO "category" ("id", "name", "activated") VALUES (14, 'Esportivo', true);
---
+++ Back-end/src/main/resources/db/data/S5__insert_propulsion.sql
@@ -0,0 +1,4 @@
+-- Inserindo tipos de propulsão na tabela "Propulsion"
+INSERT INTO "propulsion" ("id", "name", "activated") VALUES (1, 'Elétrico', true);
+INSERT INTO "propulsion" ("id", "name", "activated") VALUES (2, 'Plug-in', true);
+INSERT INTO "propulsion" ("id", "name", "activated") VALUES (3, 'Híbridos', true);
