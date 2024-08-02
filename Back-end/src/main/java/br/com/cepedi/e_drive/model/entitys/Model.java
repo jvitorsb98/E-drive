864b8183bdcd025e3a0cc2d61660448623859e92
João Ramos <jvitorsb98@outlook.com>
2024-08-02 14:41:11 -0300

    merge criando coluna model no vehicle

---
+++ Back-end/src/main/java/br/com/cepedi/e_drive/model/entitys/Model.java
@@ -0,0 +1,27 @@
+package br.com.cepedi.e_drive.model.entitys;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "model")
+public class Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "activated", nullable = false)
+    private Boolean activated;
+
+}
--- Back-end/src/main/resources/db/migration/V13__create-table-vehicle.sql
+++ Back-end/src/main/resources/db/migration/V13__create-table-vehicle.sql
@@ -2,7 +2,7 @@ CREATE TABLE vehicle (
     id BIGSERIAL PRIMARY KEY,
     category_id BIGINT NOT NULL,
     type_id BIGINT NOT NULL,
-    brand_id BIGINT NOT NULL,
+    model_id BIGINT NOT NULL,
     version VARCHAR(255) NOT NULL,
     motor VARCHAR(255) NOT NULL,
     propulsion_id BIGINT NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE vehicle (
     "year" BIGINT NOT NULL,
     FOREIGN KEY (category_id) REFERENCES category(id),
     FOREIGN KEY (type_id) REFERENCES vehicle_type(id),
-    FOREIGN KEY (brand_id) REFERENCES brand(id),
+    FOREIGN KEY (model_id) REFERENCES model(id),
     FOREIGN KEY (propulsion_id) REFERENCES propulsion(id),
     FOREIGN KEY (autonomy_id) REFERENCES autonomy(id)
 );
