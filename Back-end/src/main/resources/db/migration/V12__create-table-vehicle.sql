7e9e49853d7bff75bb838129b76c3cb387fc4092
João Ramos <jvitorsb98@outlook.com>
2024-08-02 08:46:31 -0300

    Security preprados ate service

--- Back-end/src/main/resources/db/migration/V12__create-table-vehicle.sql
+++ Back-end/src/main/resources/db/migration/V12__create-table-vehicle.sql
@@ -1,15 +1,16 @@
 CREATE TABLE vehicle (
     id BIGSERIAL PRIMARY KEY,
     category_id BIGINT NOT NULL,
-    type BIGINT NOT NULL,
+    type_id BIGINT NOT NULL,
     brand BIGINT NOT NULL,
     version VARCHAR(255) NOT NULL,
     motor VARCHAR(255) NOT NULL,
     propulsion BIGINT NOT NULL,
     autonomy_id BIGINT NOT NULL,
     activated BOOLEAN NOT NULL,
+    "year" BIGINT NOT NULL,
     FOREIGN KEY (category_id) REFERENCES category(id),
-    FOREIGN KEY (type) REFERENCES vehicle_type(id),
+    FOREIGN KEY (type_id) REFERENCES vehicle_type(id),
     FOREIGN KEY (brand) REFERENCES brand(id),
     FOREIGN KEY (propulsion) REFERENCES propulsion(id),
     FOREIGN KEY (autonomy_id) REFERENCES autonomy(id)
