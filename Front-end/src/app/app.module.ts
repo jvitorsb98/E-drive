57b6614a49ff386e0c6d8545f6884c21f84909e7
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-08-07 16:24:35 -0300

    AngularMaterialModule e AppModule

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http';
+import { AngularMaterialModule } from './settings/angular-material/angular-material.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AngularMaterialModule
   ],
   providers: [
     provideClientHydration(),
--- Front-end/src/app/settings/angular-material/angular-material.module.ts
+++ Front-end/src/app/settings/angular-material/angular-material.module.ts
@@ -1,11 +1,52 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MAT_DATE_LOCALE, MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
+// Mask para aplicar a configuração de máscara de input de texto.
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @NgModule({
-  declarations: [
-
-  ],
   imports: [
-   
-  ]
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatTableModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    NgxMaskDirective,
+  ],
+  exports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatTableModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    NgxMaskDirective,
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    provideNgxMask(),
+    provideNativeDateAdapter()
+  ],
 })
 export class AngularMaterialModule { }
