292045e38485e26d62a843ec9ec31966386f3927
snugbX <santoseverlan29@gmail.com>
2024-08-13 10:48:25 -0300

    organizando importações no app.modules

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -1,42 +1,49 @@
+// Angular Modulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularMaterialModule } from './settings/angular-material/angular-material.module';
+
+// Providers
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+// Componentes
+import { AppComponent } from './app.component';
 import { UserRegistrationFormComponent } from './settings/features/users/components/user-registration-form/user-registration-form.component';
 import { UserPasswordModalComponent } from './settings/features/users/components/user-password-modal/user-password-modal.component';
-
-// Diretivas
-import { EmailPatternValidatorDirective } from './settings/shared/directives/email-pattern-validator.directive';
-
-// Pipes
-import { PhoneMaskPipe } from './settings/shared/pipes/phone-mask.pipe';
 import { IntroPageComponent } from './settings/features/intro-page/intro-page.component';
 import { FooterComponent } from './settings/core/fragments/footer/footer.component';
 import { NavbarIntroComponent } from './settings/core/fragments/header/navbar-intro/navbar-intro.component';
 import { UserLoginModalComponent } from './settings/core/security/login/user-login-modal/user-login-modal.component';
 import { ResetPasswordComponent } from './settings/core/security/login/recover-password/reset-password/reset-password.component';
 
+// Diretivas
+import { EmailPatternValidatorDirective } from './settings/shared/directives/email-pattern-validator.directive';
+
+// Pipes
+import { PhoneMaskPipe } from './settings/shared/pipes/phone-mask.pipe';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
     UserPasswordModalComponent,
+    IntroPageComponent,
+    FooterComponent,
+    NavbarIntroComponent,
+    UserLoginModalComponent,
+    ResetPasswordComponent,
 
     // Diretivas
     EmailPatternValidatorDirective,
 
     // Pipes
     PhoneMaskPipe,
-      IntroPageComponent,
-      FooterComponent,
-      NavbarIntroComponent,
-      UserLoginModalComponent,
-      ResetPasswordComponent,
+
   ],
   imports: [
     BrowserModule,
