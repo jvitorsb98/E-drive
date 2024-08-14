e16153b3bf772514e802e5a4dd01648a2e3dc681
João Ramos <jvitorsb98@outlook.com>
2024-08-14 11:35:01 -0300

    Error email Cadastrado

--- Back-end/src/main/java/br/com/cepedi/e_drive/security/service/AuthService.java
+++ Back-end/src/main/java/br/com/cepedi/e_drive/security/service/AuthService.java
@@ -4,6 +4,7 @@ import br.com.cepedi.e_drive.security.model.entitys.User;
 import br.com.cepedi.e_drive.security.model.records.details.DataDetailsRegisterUser;
 import br.com.cepedi.e_drive.security.model.records.register.DataRegisterUser;
 import br.com.cepedi.e_drive.security.repository.UserRepository;
+import br.com.cepedi.e_drive.security.service.validations.register.ValidationRegisterUser;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AuthService implements UserDetailsService {
@@ -25,6 +28,9 @@ public class AuthService implements UserDetailsService {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private List<ValidationRegisterUser> validationRegisterUserList;
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         UserDetails user = repository.findByEmail(email); // Atualizado para buscar pelo email
@@ -35,6 +41,7 @@ public class AuthService implements UserDetailsService {
     }
 
     public DataDetailsRegisterUser register(DataRegisterUser dataRegisterUser) {
+        validationRegisterUserList.forEach(v -> v.validation(dataRegisterUser));
         User user = new User(dataRegisterUser, passwordEncoder);
         repository.save(user);
         String confirmationToken = tokenService.generateTokenForActivatedEmail(user);
---
+++ Back-end/src/main/java/br/com/cepedi/e_drive/security/service/validations/register/ValidationRegisterUser.java
@@ -0,0 +1,9 @@
+package br.com.cepedi.e_drive.security.service.validations.register;
+
+import br.com.cepedi.e_drive.security.model.records.register.DataRegisterUser;
+
+public interface ValidationRegisterUser {
+
+    void validation(DataRegisterUser dataRegisterUser);
+
+}
