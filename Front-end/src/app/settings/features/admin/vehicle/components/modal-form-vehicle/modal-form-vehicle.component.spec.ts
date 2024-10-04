9777f309b0ea03b25e2dc47115d1a2c023c2487b
IpvLeo <leoanarcopunk77@gmail.com>
2024-10-04 17:01:56 -0300

    test components ok

--- Front-end/src/app/settings/features/admin/vehicle/components/modal-form-vehicle/modal-form-vehicle.component.spec.ts
+++ Front-end/src/app/settings/features/admin/vehicle/components/modal-form-vehicle/modal-form-vehicle.component.spec.ts
@@ -1,83 +1,150 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { of, throwError } from 'rxjs';
 import { ModalFormVehicleComponent } from './modal-form-vehicle.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { of } from 'rxjs';
 import { BrandService } from '../../../../../core/services/brand/brand.service';
+import { CategoryService } from '../../../../../core/services/category/category.service';
+import { ModelService } from '../../../../../core/services/model/model.service';
+import { PropusionService } from '../../../../../core/services/propusion/propusion.service';
+import { TypeVehicleService } from '../../../../../core/services/typeVehicle/type-vehicle.service';
 import { VehicleService } from '../../../../../core/services/vehicle/vehicle.service';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { MockService } from 'ng-mocks';
 import Swal from 'sweetalert2';
 import { Vehicle } from '../../../../../core/models/vehicle';
-
-// Mock do BrandService
-class MockBrandService {
-  getAll() {
-    return of({ content: [{ id: 1, name: 'Brand 1' }, { id: 2, name: 'Brand 2' }] });
-  }
-}
-
-// Mock do VehicleService
-class MockVehicleService {
-  register(vehicleData: any) {
-    return of({}); // Simule a resposta do registro
-  }
-
-  update(id: number, vehicleData: any) {
-    return of({}); // Simule a resposta da atualização
-  }
-}
-
-// Mock do MatDialogRef
-class MockMatDialogRef {
-  close() {}
-}
-
-// Dados do mock do diálogo
-const mockDialogData = {
-  motor: 'V8',
-  version: '2023',
-  model: { brand: { name: 'Brand 1' }, name: 'Model 1' },
-  category: { name: 'Category 1' },
-  type: { name: 'Type 1' },
-  propulsion: { name: 'Propulsion 1' },
-  autonomy: {
-    mileagePerLiterCity: 10,
-    mileagePerLiterRoad: 15,
-    consumptionEnergetic: 7.8,
-    autonomyElectricMode: 100,
-  },
-  year: 2023,
-  activated: true,
-};
+import { Category } from '../../../../../core/models/category';
+import { Model } from '../../../../../core/models/model';
 
 describe('ModalFormVehicleComponent', () => {
   let component: ModalFormVehicleComponent;
   let fixture: ComponentFixture<ModalFormVehicleComponent>;
-  let vehicleService: VehicleService;
+  let brandServiceMock: Partial<BrandService>;
+  let categoryServiceMock: Partial<CategoryService>;
+  let modelServiceMock: Partial<ModelService>;
+  let propulsionServiceMock: Partial<PropusionService>;
+  let typeVehicleServiceMock: Partial<TypeVehicleService>;
+  let vehicleServiceMock: Partial<VehicleService>;
+
+  const mockVehicle: Vehicle = {
+    id: 1,
+    motor: 'Motor A',
+    version: 'Version A',
+    model: { id: 1, name: 'Model A', brand: { id: 1, name: 'Brand A', activated: true }, activated: true },
+    category: { id: 1, name: 'Category A', activated: true },
+    type: { id: 1, name: 'Type A', activated: true },
+    propulsion: { id: 1, name: 'Propulsion A', activated: true },
+    autonomy: {
+      mileagePerLiterCity: 10,
+      mileagePerLiterRoad: 12,
+      consumptionEnergetic: 15,
+      autonomyElectricMode: 20,
+    },
+    year: 2022,
+    activated: true,
+  };
 
   beforeEach(async () => {
+    // Mock dos serviços
+    brandServiceMock = {
+      getAll: jest.fn().mockReturnValue(of({ content: [{ id: 1, name: 'Brand A' }] })),
+    };
+
+    categoryServiceMock = {
+      getAll: jest.fn().mockReturnValue(of({ content: [{ id: 1, name: 'Category A' }] })),
+    };
+
+    modelServiceMock = {
+      getModelsByBrandId: jest.fn().mockReturnValue(of({ content: [{ id: 1, name: 'Model A' }] })),
+    };
+
+    propulsionServiceMock = {
+      getAll: jest.fn().mockReturnValue(of({ content: [{ id: 1, name: 'Propulsion A' }] })),
+    };
+
+    typeVehicleServiceMock = {
+      getAll: jest.fn().mockReturnValue(of({ content: [{ id: 1, name: 'Type A' }] })),
+    };
+
+    vehicleServiceMock = {
+      register: jest.fn(),
+      update: jest.fn(),
+    };
+
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [ReactiveFormsModule],
       declarations: [ModalFormVehicleComponent],
       providers: [
-        { provide: BrandService, useClass: MockBrandService },
-        { provide: VehicleService, useClass: MockVehicleService },
-        { provide: MatDialogRef, useClass: MockMatDialogRef },
-        { provide: MAT_DIALOG_DATA, useValue: mockDialogData },
+        { provide: BrandService, useValue: brandServiceMock },
+        { provide: CategoryService, useValue: categoryServiceMock },
+        { provide: ModelService, useValue: modelServiceMock },
+        { provide: PropusionService, useValue: propulsionServiceMock },
+        { provide: TypeVehicleService, useValue: typeVehicleServiceMock },
+        { provide: VehicleService, useValue: vehicleServiceMock },
+        { provide: MatDialogRef, useValue: { close: jest.fn() } },
+        { provide: MAT_DIALOG_DATA, useValue: { motor: '', version: '', model: {}, category: {}, type: {}, propulsion: {}, autonomy: {}, year: 2022, activated: true } },
       ],
-      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ModalFormVehicleComponent);
     component = fixture.componentInstance;
-    vehicleService = TestBed.inject(VehicleService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should initialize form correctly', () => {
+    component.ngOnInit();
+    expect(component.vehicleForm).toBeTruthy();
+    expect(component.vehicleForm.get('motor')).toBeTruthy();
+  });
+
+  it('should load brands on init', () => {
+    component.ngOnInit();
+    expect(brandServiceMock.getAll).toHaveBeenCalled();
+    expect(component.brands.length).toBeGreaterThan(0);
+  });
+
+  it('should load categories on init', () => {
+    component.ngOnInit();
+    expect(categoryServiceMock.getAll).toHaveBeenCalled();
+    expect(component.categories.length).toBeGreaterThan(0);
+  });
+
+  it('should load models when a brand is selected', () => {
+    component.ngOnInit();
+    component.vehicleForm.get('brand')?.setValue('Brand A');
+    expect(modelServiceMock.getModelsByBrandId).toHaveBeenCalledWith(1); // Verifique se o ID da marca está correto
+  });
+
+  it('should submit the form and call register', async () => {
+    component.ngOnInit();
+    component.vehicleForm.patchValue({
+      motor: 'Motor A',
+      version: 'Version A',
+      brand: 'Brand A',
+      model: 'Model A',
+      category: 'Category A',
+      type: 'Type A',
+      propulsion: 'Propulsion A',
+      mileagePerLiterCity: 10,
+      mileagePerLiterRoad: 12,
+      consumptionEnergetic: 15,
+      autonomyElectricMode: 20,
+      year: 2022,
+    });
+
+    jest.spyOn(vehicleServiceMock, 'register').mockReturnValue(of(mockVehicle)); // Simule sucesso na resposta
+
+    await component.submitForm();
+
+    expect(vehicleServiceMock.register).toHaveBeenCalled();
+  });
+
+  it('should close the dialog on reset', () => {
+    component.closeModal();
+    expect(TestBed.inject(MatDialogRef).close).toHaveBeenCalled();
+  });
 });
--- Front-end/src/app/settings/features/admin/vehicle/components/vehicle-list/vehicle-list.component.spec.ts
+++ Front-end/src/app/settings/features/admin/vehicle/components/vehicle-list/vehicle-list.component.spec.ts
@@ -1,23 +1,146 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { VehicleListComponent } from './vehicle-list.component';
+import { VehicleService } from '../../../../../core/services/vehicle/vehicle.service';
+import { AlertasService } from '../../../../../core/services/Alertas/alertas.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { of, throwError } from 'rxjs';
+import { PaginatedResponse } from '../../../../../core/models/paginatedResponse';
+import { Vehicle } from '../../../../../core/models/vehicle';
+import { HttpErrorResponse } from '@angular/common/http';
+import { IAutonomyRequest } from '../../../../../core/models/autonomy';
+
+// Mocks para VehicleType e Propulsion
+const vehicleTypeMock = { id: 1, name: 'SEDAN', activated: true };
+const propulsionMock = { id: 1, name: 'PETROL', activated: true };
 
 describe('VehicleListComponent', () => {
   let component: VehicleListComponent;
   let fixture: ComponentFixture<VehicleListComponent>;
+  let vehicleService: jest.Mocked<VehicleService>;
+  let alertService: jest.Mocked<AlertasService>;
+  let dialog: jest.Mocked<MatDialog>;
+
+  const vehicleMock: Vehicle = {
+    id: 1,
+    type: vehicleTypeMock,  // Usando o mock correto
+    motor: '1.0',
+    version: 'Versão 1',
+    year: 2020,
+    autonomy: {
+      mileagePerLiterRoad: 15,
+      mileagePerLiterCity: 10,
+      consumptionEnergetic: 2,
+      autonomyElectricMode: 100,
+    } as IAutonomyRequest,
+    propulsion: propulsionMock,  // Usando o mock correto
+    model: { id: 1, name: 'Modelo 1', brand: { id: 1, name: 'Marca 1', activated: true }, activated: true },
+    category: { id: 1, name: 'Categoria 1', activated: true },
+    activated: true,
+  };
+
+  const paginatedResponseMock: PaginatedResponse<Vehicle> = {
+    content: [vehicleMock],
+    totalElements: 1,
+    pageable: {
+      pageNumber: 0,
+      pageSize: 5,
+      offset: 0,
+      sort: {
+        empty: false,
+        sorted: false,
+        unsorted: true,
+      },
+      paged: true,
+      unpaged: false,
+    },
+    numberOfElements: 1,
+    size: 5,
+    sort: { active: '', direction: '' },  // Usando o MatSort correto
+    totalPages: 1,
+    last: true,
+    first: true,
+    number: 0,
+    empty: false,
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [VehicleListComponent]
-    })
-    .compileComponents();
-    
+      declarations: [VehicleListComponent],
+      providers: [
+        { provide: VehicleService, useValue: { getAll: jest.fn(), activate: jest.fn(), deactivate: jest.fn() } },
+        { provide: AlertasService, useValue: { showError: jest.fn(), showSuccess: jest.fn() } },
+        { provide: MatDialog, useValue: { open: jest.fn().mockReturnValue({ afterClosed: jest.fn().mockReturnValue(of(true)) }) } },
+        { provide: MatPaginator, useValue: {} },
+        { provide: MatSort, useValue: {} },
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(VehicleListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    vehicleService = TestBed.inject(VehicleService) as jest.Mocked<VehicleService>;
+    alertService = TestBed.inject(AlertasService) as jest.Mocked<AlertasService>;
+    dialog = TestBed.inject(MatDialog) as jest.Mocked<MatDialog>;
+
+    // Mock para o método getAll do serviço VehicleService
+    jest.spyOn(vehicleService, 'getAll').mockReturnValue(of(paginatedResponseMock));
+    
+    component.ngOnInit(); 
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should fetch the list of vehicles on init', () => {
+    component.getList(0, 5);
+    expect(vehicleService.getAll).toHaveBeenCalledWith(0, 5);
+    expect(component.dataSource.data).toEqual([vehicleMock]);
+  });
+
+
+  it('should activate a vehicle', () => {
+    jest.spyOn(vehicleService, 'activate').mockReturnValue(of({}));
+
+    component.activate(vehicleMock);
+    expect(vehicleService.activate).toHaveBeenCalledWith(vehicleMock.id);
+    expect(alertService.showSuccess).toHaveBeenCalledWith("Sucesso !!", "Ativado com sucesso");
+  });
+
+  it('should deactivate a vehicle', () => {
+    jest.spyOn(vehicleService, 'deactivate').mockReturnValue(of({}));
+
+    component.deactivate(vehicleMock);
+    expect(vehicleService.deactivate).toHaveBeenCalledWith(vehicleMock.id);
+    expect(alertService.showSuccess).toHaveBeenCalledWith("Sucesso !!", "Desativado com sucesso");
+  });
+
+  it('should open the add vehicle modal', () => {
+    component.openModalAdd();
+    expect(dialog.open).toHaveBeenCalled();
+  });
+
+  it('should open the edit vehicle modal', () => {
+    component.openModalEdit(vehicleMock);
+    expect(dialog.open).toHaveBeenCalledWith(expect.anything(), {
+      width: '99%',
+      height: '80%',
+      data: vehicleMock,
+    });
+  });
+  it('should fetch the list of vehicles on successful response', () => {
+    jest.spyOn(vehicleService, 'getAll').mockReturnValue(of(paginatedResponseMock));
+
+    component.getList(0, 5);
+
+    expect(vehicleService.getAll).toHaveBeenCalledWith(0, 5);
+    expect(component.dataSource.data).toEqual([vehicleMock]); // Verifica se a dataSource está correta
+    expect(component.totalVehicles).toBe(1); // Verifica se totalVehicles está correto
+  });
+
+ 
+
 });
