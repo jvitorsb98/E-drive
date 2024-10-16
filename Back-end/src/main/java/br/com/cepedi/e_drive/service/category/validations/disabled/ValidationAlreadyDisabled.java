6e603d7ebf1514f3744033c4599a52c0fc79e402
danilo775 <danilo.uba@hotmail.com>
2024-10-16 13:20:18 -0300

    add internacionalização de Category

--- Back-end/src/main/java/br/com/cepedi/e_drive/service/category/validations/disabled/ValidationAlreadyDisabled.java
+++ Back-end/src/main/java/br/com/cepedi/e_drive/service/category/validations/disabled/ValidationAlreadyDisabled.java
@@ -3,7 +3,10 @@ package br.com.cepedi.e_drive.service.category.validations.disabled;
 import br.com.cepedi.e_drive.model.entitys.Category;
 import br.com.cepedi.e_drive.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
 
 /**
  * Classe de validação que verifica se uma categoria já está desativada.
@@ -16,6 +19,9 @@ public class ValidationAlreadyDisabled implements CategoryValidatorDisabled {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private MessageSource messageSource; // Injeção de MessageSource para internacionalização
+
     /**
      * Valida se a categoria já está desativada.
      *
@@ -25,9 +31,22 @@ public class ValidationAlreadyDisabled implements CategoryValidatorDisabled {
     @Override
     public void validate(Long id) {
         Category category = categoryRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Category does not exist."));
+                .orElseThrow(() -> {
+                    String errorMessage = messageSource.getMessage(
+                            "category.disabled.not.found", // Chave da mensagem
+                            new Object[]{id}, // Parâmetros da mensagem
+                            Locale.getDefault() // Locale padrão
+                    );
+                    return new IllegalArgumentException(errorMessage);
+                });
+
         if (!category.getActivated()) {
-            throw new IllegalArgumentException("Category is already disabled.");
+            String errorMessage = messageSource.getMessage(
+                    "category.disabled.already.disabled", // Chave da mensagem
+                    new Object[]{id}, // Parâmetros da mensagem
+                    Locale.getDefault() // Locale padrão
+            );
+            throw new IllegalArgumentException(errorMessage);
         }
     }
 }
