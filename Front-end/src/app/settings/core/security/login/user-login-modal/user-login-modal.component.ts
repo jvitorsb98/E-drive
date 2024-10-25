09aa52b2da89fe2667d32545c94aa1572e8f3900
snugbX <santoseverlan29@gmail.com>
2024-10-25 14:18:33 -0300

    imagens na tela de login

--- Front-end/src/app/settings/core/security/login/user-login-modal/user-login-modal.component.ts
+++ Front-end/src/app/settings/core/security/login/user-login-modal/user-login-modal.component.ts
@@ -20,6 +20,10 @@ export class UserLoginModalComponent implements OnInit {
   loginForm!: FormGroup;
   isLoading = false;
   successMessage: string | null = null;
+  //TODO - remoção de opção de login com imagem
+  // userProfileImage = 'assets/images/Gemini_Generated_Image_dda4t6dda4t6dda4.jpeg';  // Caminho da imagem
+  userProfileImage = 'assets/images/DALL·E 2024-10-17.jpeg';
+  // userProfileImage = 'assets/images/Gemini_Generated_Image_lhxmx1lhxmx1lhxm.jpeg';
 
   constructor(
     private fb: FormBuilder,
@@ -62,7 +66,7 @@ export class UserLoginModalComponent implements OnInit {
   modalResetPassword(isPasswordRecovery: boolean): void {
     this.modal.openModal(ModalRecoverPasswordComponent, {
       email: this.loginForm.get('email')?.value,
-      isPasswordRecovery: isPasswordRecovery  
+      isPasswordRecovery: isPasswordRecovery
     }, {
       width: '500px',
       height: '250px',
