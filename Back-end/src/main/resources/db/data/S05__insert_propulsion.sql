1e57a5b99afaf430a491f4238a142df428b967cf
João Ramos <jvitorsb98@outlook.com>
2024-11-06 13:25:02 -0300

    Correção dos insert into

--- Back-end/src/main/resources/db/data/S05__insert_propulsion.sql
+++ Back-end/src/main/resources/db/data/S05__insert_propulsion.sql
@@ -1,4 +1,13 @@
--- Inserindo tipos de propulsão na tabela "Propulsion"
-INSERT INTO "propulsion" ("name", "activated") VALUES ('Elétrico', true);
-INSERT INTO "propulsion" ("name", "activated") VALUES ('Plug-in', true);
-INSERT INTO "propulsion" ("name", "activated") VALUES ('Híbrido', true);
+-- Verifica se a tabela "propulsion" está vazia
+DO '
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM propulsion) THEN
+        -- Inserindo tipos de propulsão na tabela "propulsion"
+        INSERT INTO "propulsion" ("name", "activated") VALUES 
+            (''Elétrico'', true),
+            (''Plug-in'', true),
+            (''Híbrido'', true);
+    END IF;
+END
+';
+
