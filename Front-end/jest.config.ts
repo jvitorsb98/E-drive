1a261f961f66c3d4676937ab1469a337f1464a2b
João Ramos <jvitorsb98@outlook.com>
2024-11-18 15:42:57 -0300

    Jest

--- Front-end/jest.config.ts
+++ Front-end/jest.config.ts
@@ -4,10 +4,15 @@ const config: Config = {
   coverageDirectory: 'coverage', // Diretório para os relatórios de cobertura
   coverageReporters: ['html', 'text', 'lcov'], // Formatos dos relatórios de cobertura
   collectCoverageFrom: [
-    "**/*.{js,jsx,ts,tsx}", // Inclui arquivos .ts e .tsx
-    "!**/node_modules/**",
-    "!**/vendor/**",
-    "!**/*.spec.{js,jsx,ts,tsx}", // Exclui arquivos de teste
+    "**/*.{js,jsx,ts,tsx}", // Inclui arquivos principais
+    "!**/node_modules/**",  // Ignora node_modules
+    "!**/vendor/**",        // Ignora vendor
+    "!**/dist/**",          // Ignora build final
+    "!**/*.spec.{js,jsx,ts,tsx}", // Ignora arquivos de teste
+    "!**/.angular/**",      // Ignora a pasta de cache do Angular
+    "!**/coverage/**",      // Ignora a pasta de relatórios de cobertura
+    "!<rootDir>/setup-jest.ts",  // Ignora o arquivo de setup do Jest
+    "!**/*.config.{js,ts}", // Ignora arquivos de configuração
   ],
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
