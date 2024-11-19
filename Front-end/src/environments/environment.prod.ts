0458be26b77d14a50150a0fee73daaf126ebae13
João Ramos <jvitorsb98@outlook.com>
2024-11-19 14:05:00 -0300

    enviments

--- Front-end/src/environments/environment.prod.ts
+++ Front-end/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080', 
+  apiUrl: 'http://54.200.23.253:8080', 
   googleMapsApiKey: 'AIzaSyCVzPB0CvNAoAR96FJ0pPrlQBXcO4v9yZs' // Substitua pela chave de API
 };
