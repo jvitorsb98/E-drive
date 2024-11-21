1287a2df5a1396cf09d05823d958a37f8d9d29a9
João Ramos <jvitorsb98@outlook.com>
2024-11-20 23:31:39 -0300

    teste requisição http para o backend

--- Front-end/src/environments/environment.prod.ts
+++ Front-end/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://54.200.23.253:8080', // Use localhost para comunicar-se com o backend
+  apiUrl: 'http://54.200.23.253:8080', // Use localhost para comunicar-se com o backend
   googleMapsApiKey: 'AIzaSyCVzPB0CvNAoAR96FJ0pPrlQBXcO4v9yZs' // Substitua pela chave de API
 };
