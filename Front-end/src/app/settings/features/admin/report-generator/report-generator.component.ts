722bcb32b3135687bd4e216ad481aabc3c06b061
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-11-27 14:58:21 -0300

    Component comentado

--- Front-end/src/app/settings/features/admin/report-generator/report-generator.component.ts
+++ Front-end/src/app/settings/features/admin/report-generator/report-generator.component.ts
@@ -1,9 +1,10 @@
 import { Component, ViewChild } from '@angular/core';
-import { ReportService } from '../../../core/services/report/report.service';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { map, Observable, of, startWith } from 'rxjs';
 import { MatAutocompleteTrigger } from '@angular/material/autocomplete';
 import { Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { ReportService } from '../../../core/services/report/report.service';
 import { AlertasService } from '../../../core/services/Alertas/alertas.service';
 
 @Component({
@@ -13,6 +14,7 @@ import { AlertasService } from '../../../core/services/Alertas/alertas.service';
 })
 export class ReportGeneratorComponent {
   @ViewChild(MatAutocompleteTrigger) autocompleteTrigger!: MatAutocompleteTrigger;
+
   reportForm!: FormGroup; // Formulário para seleção de relatórios
   noReportFound: boolean = false; // Indica se nenhum relatório foi encontrado
   reports = [ // Lista mockada de relatórios
@@ -28,25 +30,30 @@ export class ReportGeneratorComponent {
   filteredReports: Observable<{ id: number; name: string }[]> = of([]); // Lista de relatórios filtrada para o autocomplete
 
   /**
-   * Construtor da classe `ReportGeneratorComponent`
-   * Injeta a dependência do serviço `ReportService` e do `FormBuilder` para criar formulários.
-   * @param reportService - Serviço responsável por gerar relatórios.
-   * @param formBuilder - FormBuilder para criar formulários reativos.
-   * @param router - Router para navegação entre páginas.
+   * @description Construtor da classe `ReportGeneratorComponent`
+   * Injeta as dependências necessárias para o funcionamento do componente.
+   * @param reportService Serviço responsável por gerar relatórios.
+   * @param formBuilder FormBuilder para criar formulários reativos.
+   * @param alertService Serviço de alertas.
+   * @param router Router para navegação entre páginas.
    */
   constructor(
     private reportService: ReportService,
     private formBuilder: FormBuilder,
     private alertService: AlertasService,
-    private router: Router,) { }
+    private router: Router
+  ) { }
 
+  /**
+   *@description Método de ciclo de vida do Angular chamado ao inicializar o componente.
+   */
   ngOnInit(): void {
     this.buildForm();
     this.setupAutocomplete();
   }
 
   /**
-   * Inicializa o formulário para seleção de relatórios.
+   * @description Inicializa o formulário para seleção de relatórios.
    */
   private buildForm() {
     this.reportForm = this.formBuilder.group({
@@ -54,6 +61,9 @@ export class ReportGeneratorComponent {
     });
   }
 
+  /**
+   * @description Configura o comportamento de autocomplete do campo de relatório.
+   */
   setupAutocomplete() {
     this.filteredReports = this.reportForm.get('report')!.valueChanges.pipe(
       startWith(''),
@@ -68,77 +78,69 @@ export class ReportGeneratorComponent {
   }
 
   /**
- * Função para exibir o nome do relatório no campo do autocomplete.
- * @param report Objeto do relatório selecionado.
- * @returns Nome do relatório ou uma string vazia.
- */
+   * @description Exibe o nome do relatório selecionado no campo do autocomplete.
+   * @param report Objeto do relatório selecionado.
+   * @returns Nome do relatório ou uma string vazia.
+   */
   displayReportName(report: { id: number; name: string } | null): string {
     return report ? report.name : '';
   }
 
   /**
-   * Método chamado ao submeter o formulário de relatórios.
-   * @param reportId - ID do relatório selecionado.
+   * @description Método chamado ao submeter o formulário de relatórios.
    */
   gerarReport(): void {
-    const selectedReport = this.reportForm.get('report')?.value; // Obter o relatório selecionado
+    const selectedReport = this.reportForm.get('report')?.value;
     switch (selectedReport.id) {
       case 1:
         this.loadMostRegisteredCarsReport();
         break;
-
       default:
         this.alertService.showWarning('Relatório não encontrado', 'O relatório selecionado não foi encontrado.');
     }
   }
 
   /**
- * Método para carregar o relatório de carros mais registrados
- * Chama o serviço que obtém o relatório e abre o PDF em uma nova aba do navegador.
- */
+   * @description Carrega o relatório de carros mais registrados.
+   */
   loadMostRegisteredCarsReport() {
-    // Faz a requisição para obter o relatório do serviço
     this.reportService.getMostRegisteredCarsReport().subscribe({
-      // Caso a requisição seja bem-sucedida, abre o relatório em uma nova aba
       next: (data) => this.openInNewTab(data),
-      // Caso ocorra um erro, exibe a mensagem no console
       error: (err) => console.error('Erro ao gerar relatório:', err)
     });
   }
 
   /**
-   * Método para abrir o arquivo em uma nova aba do navegador
-   * @param blob - O arquivo binário (PDF) retornado pela requisição
+   * @description Abre o relatório em uma nova aba do navegador.
+   * @param blob O arquivo binário (PDF) retornado pela requisição.
    */
   private openInNewTab(blob: Blob): void {
-    // Cria uma URL temporária para o Blob
     const blobUrl = window.URL.createObjectURL(blob);
-
-    // Abre a URL gerada em uma nova aba do navegador
     window.open(blobUrl, '_blank');
-
-    // Libera a memória usada pela URL temporária
     window.URL.revokeObjectURL(blobUrl);
   }
 
   /**
-   * @description Alternar a visibilidade do autocomplete.
+   * @description Alterna a visibilidade do painel de autocomplete.
    * @param event Evento disparado pelo clique do botão.
    */
   toggleAutocomplete(event: Event) {
-    event.stopPropagation(); // Previne a propagação do evento, para evitar o fechamento inesperado
-
-    // Abra o painel sempre, independentemente dos resultados
+    event.stopPropagation();
     if (!this.autocompleteTrigger.panelOpen) {
-      this.autocompleteTrigger.openPanel();  // Abre o painel
+      this.autocompleteTrigger.openPanel();
     } else {
-      this.autocompleteTrigger.closePanel();  // Se já estiver aberto, fecha
+      this.autocompleteTrigger.closePanel();
     }
   }
 
+  /**
+   * @description Verifica se nenhum relatório foi encontrado com base no valor filtrado.
+   * @param value Valor do campo de autocomplete.
+   * @param filtered Lista filtrada de relatórios.
+   */
   checkNoReportFound(value: string | { id: number; name: string }, filtered: any[]) {
-    if (!value || typeof value === 'string' && value.trim() === '') {
-      this.noReportFound = true; // Campo vazio, desabilitar o botão
+    if (!value || (typeof value === 'string' && value.trim() === '')) {
+      this.noReportFound = true;
     } else if (typeof value === 'string') {
       this.noReportFound = filtered.length === 0;
     } else {
@@ -146,8 +148,10 @@ export class ReportGeneratorComponent {
     }
   }
 
+  /**
+   * @description Navega de volta para a dashboard do E-Driver.
+   */
   goBack(): void {
-    this.router.navigate(["/e-driver/dashboard"]);
+    this.router.navigate(['/e-driver/dashboard']);
   }
-
 }
